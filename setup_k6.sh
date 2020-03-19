a3412ae0f1f2bb70240e1a7c209178ad79491810
Ajithkumar10 <ajit.bmsit@gmail.com>
2020-03-19 19:01:58 +0400

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -3,7 +3,6 @@ set -ex
 
 install_k6() {
     apt-get update
-    apt-get install dirmngr --install-recommends
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
     echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
     apt-get update
